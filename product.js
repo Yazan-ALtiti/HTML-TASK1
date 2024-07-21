2e24a6a9c12bc89d311e0f089cc94fbac3ba16e0
Yazan-ALtiti <titiyazan14@gmail.com>
2024-07-21 08:18:44 +0300

    update

---
+++ product.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("form");
+const table = document.getElementById("table");
+const tbody = document.createElement("tbody");
+table.appendChild(tbody);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let name = e.target.name.value;
+  let email = e.target.email.value;
+  let car = e.target.car.value;
+  let price = e.target.price.value;
+
+  let tr = document.createElement("tr");
+
+  let tdName = document.createElement("td");
+  tdName.textContent = name;
+  tr.appendChild(tdName);
+
+  let tdEmail = document.createElement("td");
+  tdEmail.textContent = email;
+  tr.appendChild(tdEmail);
+
+  let tdCar = document.createElement("td");
+  tdCar.textContent = car;
+  tr.appendChild(tdCar);
+
+  let tdPrice = document.createElement("td");
+  tdPrice.textContent = price;
+  tr.appendChild(tdPrice);
+
+  tbody.appendChild(tr);
+});
